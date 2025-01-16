bffdf23ce54b02d8ae04f33c39f15552baa40088
aessary <raressilviulazar@gmail.com>
2025-01-16 12:50:35 +0000

    Create Collections - Added ShadCN form

--- borcella_admin/components/collections/CollectionForm.tsx
+++ borcella_admin/components/collections/CollectionForm.tsx
@@ -1,13 +1,84 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 import { Separator } from '@/components/ui/separator'
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "../ui/textarea"
+
+const formSchema = z.object({
+    title: z.string().min(2).max(50),
+    description: z.string().min(2).max(500),
+    image: z.string(),
+  })
+
+
 
 const CollectionForm = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+            description: "",
+            image: "",
+        },
+      })
+
+
+      const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+
   return (
     <div className="p-10">
       <p className='text-heading2-bold'>Create Collection</p>
       <Separator className="bg-blue-1 mt-4 mb-7" />
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Title" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Description" {...field} rows={5} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
     </div>
-  )
-}
+  );
+};
 
 export default CollectionForm
